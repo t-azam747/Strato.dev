94544a090ed298135046f58900856bb7bc9620dd
MohakGupta2004 <mohakgupta500@gmail.com>
2025-03-29 18:33:11 +0530

    update pushing it so that everyone can access it

--- api/src/app.ts
+++ api/src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "*",
   credentials: true 
 }))
 app.use('/api/v1/auth', authRouter)
